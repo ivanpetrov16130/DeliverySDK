a1148b50e03f334bad969b8000aa2d54719a4fc1
Ilya Baykov <ily.boykov@x5.ru>
2022-01-31 16:17:02 +0300

    Upd dep

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-         .package(url: "https://github.com/ivanpetrov16130/DeliveryCore", branch: "main"),
+         .package(url: "https://github.com/ivanpetrov16130/DeliveryCore", branch: "release/1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
